8834af12c0f78836dd686425276e44823856228f
Faheem-Siddiqi <faheem_siddiqi@yahoo.com>
2024-08-14 14:17:59 +0500

    fix: vercel deployment

--- src/components/letters/Approval.jsx
+++ src/components/letters/Approval.jsx
@@ -1,19 +1,15 @@
 import { SlEnvolopeLetter } from "react-icons/sl";
 import React from 'react';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
 import pdfMake from 'pdfmake/build/pdfmake';
-
-
-pdfMake.vfs = pdfFonts.pdfMake.vfs; // Set the built-in fonts
-pdfMake.fonts = {
-    Roboto: {
-        normal: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
-        bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf",
-        italics: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf",
-        bolditalics: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf",
-    },
+// Define the fonts for pdfMake
+(pdfMake).fonts = {
+  Roboto: {
+    normal: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf",
+    bold: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf",
+    italics: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf",
+    bolditalics: "https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf",
+  },
 };
-
 const Approval = ({ title, GroupLead, PropossalID, approvalErcMember }) => {
     console.log(approvalErcMember);
 
